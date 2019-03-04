cf11135e712d073dfa34f2d1f5d7033eb67ac5af
mongooma <rhythm_qing@hotmail.com>
2019-03-04 16:15:43 -0500

    extract all entries href

--- main.py
+++ main.py
@@ -1,65 +1,36 @@
-import bs4
 import lxml
+import lxml.html
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import urllib.request
+from urllib.request import urlopen, Request
 
 
-def download(path):
-	# download the page to local file
-	with open("%s.txt" % (path.split('/')[-1]), 'w') as f
-		f.write(str(urllib.request.urlopen(path).read()))
+def extract():
 
-def search(path):
-	try:
+	t = lxml.html.fromstring(open("mainpage.html").read()) # 
 
-		return 1
-	except:
-		return 0
+	# print(t.xpath("/html/body/div/div[3]/ul[*]/li[*]/ul/li[*]/a/@href"))
+	# entries - XPATH: 
+	# /html/body/div/div[3]/ul[1]/li[24]/ul/li[1]/a
+	# /html/body/div/div[3]/ul[1]/li[1]/a
 
-def extract(soup):
-	i = 0
-	j = 0
-	k = 0
-	l = 0
+	entries = t.xpath("/html/body/div/div[3]/ul[*]/li[*]/ul/li[*]/a/@href") + \
+			 t.xpath("/html/body/div/div[3]/ul[*]/li[*]/a/@href")
 
-	found = -1
-	bottom = False;
-	while(1):
-		found = search("/html/body/div/div[3]/ul[%s]" % i) # content
-		if found:
-			while(1):
-				found = search("/html/body/div/div[3]/ul[%s]/li[%s]" % (i, j))
-				if found:
-					while(1):
-						found = search("/html/body/div/div[3]/ul[%s]/li[%s]/ul/li[%s]" % (i, j, k))
-						if found:
-							bottom = True # find a second level list
-							download("/html/body/div/div[3]/ul[%s]/li[%s]/ul/li[%s]/a.href" % (i, j, k))
-						else:
-							break
-						k += 1
-
-					if bottom == False： # only have the first level list
-						download("/html/body/div/div[3]/ul[%s]/li[%s]/a.href" % (i, j))
-				else:
-					break
-				j += 1
-		else:
-			break
-		i += 1
-
+	return entries
 
 
 
 if __name__ == "__main__":
 
-	# get the main page of SEP
+	entries = extract()
+	
 
-	main_p = "https://plato.stanford.edu/contents.html"
-	# soup = bs4.BeautifulSoup(urllib.request.urlopen(main_p).read())
-	content = open("mainpage.html").read()
 
-	# entries - XPATH: 
-	# /html/body/div/div[3]/ul[1]/li[24]/ul/li[1]/a
-	# /html/body/div/div[3]/ul[1]/li[1]/a
 
 
 
